6902e64c87cef4932277700cd6ff5fcaf6fde62d
SG <sophie.girard@ac-grenoble.fr>
2024-02-05 14:58:36 +0100

    Version initiale

---
+++ WinScrabble/Joueur.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinScrabble
+{
+    /// <summary>
+    /// Classe Joueur
+    /// </summary>
+    public class Joueur
+    {
+        #region attributs privés
+        private string nom;             // nom du joueur 
+        private List<string> lesMots;   // liste des mots proposés par le joueur
+        private int totalPoints;        // nombre total de points du joueur
+        #endregion
+
+        #region constructeur
+        /// <summary>
+        /// Constructeur de la classe Joueur
+        /// </summary>
+        /// <param name="unNom"></param>
+        public Joueur(string unNom)
+        {
+            this.nom = unNom;
+            this.lesMots = new List<string>();
+            this.totalPoints = 0;
+        }
+        #endregion
+
+        #region méthodes
+        /// <summary>
+        /// Méthode qui permet d'ajouter un mot à la liste des mots du joueur
+        /// et qui actualise le nombre total de points du joueur
+        /// </summary>
+        /// <param name="unMot">mot proposé par le joueur</param>
+        public void AjouterMot(string unMot)
+        {
+            //TODO
+        }
+
+        /// <summary>
+        /// retourne le nombre total de points du joueur
+        /// </summary>
+        /// <returns>nombre total de points du joueur</returns>
+        public int GetTotalPoints()
+        {
+            //TODO
+            return 0;
+        }
+
+        /// <summary>
+        /// retourne le nombre de mots du joueur
+        /// </summary>
+        /// <returns>nombre de mots du joueur</returns>
+        public int GetNbMots()
+        {
+            //TODO
+            return 0;
+        }
+        /// <summary>
+        /// retourne la liste des mots du joueur
+        /// </summary>
+        /// <returns>liste de mots du joueur</returns>
+        public List<string> GetLesMots()
+        {
+            //TODO
+            return null;
+        }
+
+        /// <summary>
+        /// retourne le mot qui a rapporté le plus grand nombre de points 
+        /// parmi les mots proposés par le joueur
+        /// </summary>
+        /// <returns>mot qui a rapporté le plus grand nombre de points</returns>
+        public string MotMeilleur()
+        {
+            //TODO
+            return "";
+        }
+        #endregion
+    }
+}
---
+++ WinScrabble/Utilitaire.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinScrabble
+{
+    /// <summary>
+    /// Classe qui regroupe les fonctions de calcul
+    /// </summary>
+    public static class Utilitaire
+    {
+        /// <summary>
+        /// Méthode qui retourne le nombre de points que rapporte une lettre
+        /// </summary>
+        /// <param name="l">Lettre de type char</param>
+        /// <returns>Nombre de points rapportés par la lettre</returns>
+        private static int PointsLettre(char l)
+        {
+            int p;
+            if (l == 'D' || l == 'G' || l == 'M')
+                p = 2;
+            else
+            {
+                if (l == 'B' || l == 'C' || l == 'P')
+                    p = 3;
+                else
+                {
+                    if (l == 'F' || l == 'H' || l == 'V')
+                        p = 4;
+                    else
+                    {
+                        if (l == 'J' || l == 'Q')
+                            p = 8;
+                        else
+                        {
+                            if (l == 'K' || l == 'W' || l == 'X' || l == 'Y' || l == 'Z')
+                                p = 10;
+                            else
+                                p = 1;
+                        }
+                    }
+                }
+            }
+            return p;
+        }
+        /// <summary>
+        /// Méthode qui retourne le nombre de points que rapporte un mot
+        /// </summary>
+        /// <param name="mot">Mot de type string</param>
+        /// <returns>Nombre de points du mot</returns>
+        public static int PointsMot(string mot)
+        {
+            mot = mot.ToUpper();
+            int pts = 0;
+            for (int i = 0; i <= mot.Length - 1; i++)
+            {
+                char lettre = mot[i];
+                pts = pts + PointsLettre(lettre);
+            }
+            return pts;
+        }
+
+    }
+}
